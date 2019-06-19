4e84471422eb8c6850ade386d1efa55b59b16a72
MisterL2 <46602881+MisterL2@users.noreply.github.com>
2019-06-19 16:04:59 +0200

    Nachrichten zur DB hinzugefügt

--- setupDatabase.py
+++ setupDatabase.py
@@ -6,7 +6,7 @@ cursor = conn.cursor()
 
 cursor.execute('''CREATE TABLE festungen (festung TEXT PRIMARY KEY, fraktion TEXT,
                 FOREIGN KEY (fraktion) REFERENCES fraktionen(fraktion))''')
-cursor.execute("CREATE TABLE fraktionen (fraktion TEXT PRIMARY KEY, rollen_id TEXT, leiter_id TEXT, leiter_rollen_id TEXT)") #Ids müssen TEXT sein, weil Discord IDs größer als das Integer-limit von sqlite sind!
+cursor.execute("CREATE TABLE fraktionen (fraktion TEXT PRIMARY KEY, rollen_id TEXT, leiter_id TEXT, leiter_rollen_id TEXT, nachricht TEXT)") #Ids müssen TEXT sein, weil Discord IDs größer als das Integer-limit von sqlite sind!
 cursor.execute('''CREATE TABLE invasion (festung TEXT, angreifer_fraktion TEXT, verteidiger_fraktion TEXT, datum TEXT, uhrzeit TEXT,
                 FOREIGN KEY (angreifer_fraktion) REFERENCES fraktionen(fraktion), FOREIGN KEY (verteidiger_fraktion) REFERENCES fraktionen(fraktion))''')
 cursor.execute("CREATE TABLE logs (person TEXT, person_id TEXT, command TEXT, channel TEXT, datum TEXT, uhrzeit TEXT)") #Name und ID, um exploit durch renames zu verhindern
@@ -14,17 +14,72 @@ cursor.execute("CREATE TABLE logs (person TEXT, person_id TEXT, command TEXT, ch
 
 
 #Name, RollenID, LeiterID, LeiterRollenID
-fraktionen = [("Nordmänner","587376412625731614","442350475950424104","587649118978179072"),
-              ("Wilder Bergstamm","587376373849522257", "184385677301907456", "587648048641867776"),
-              ("Piraten","587376215162355743","231855253564162049","587648225360740384"),
-              ("Ägypter","587376337077927956","534021186439610389","587648422761463818"),
-              ("Ureinwohner","587376464815456398","264177403188871178","587648931920347136"),
-              ("Mystischer Orden","587376621846265896","367732762419003403","587648869551308801"),
-              ("Römer","587376695691051017","339098056408432640","587648819680903171"),
-              ("Dunkelritter","587376791170187274","351634859082645505","587648591548383232"),
-              ("Samurai","587376876184666123","220558436037820417","587648705990098947")]
+fraktionen = [("Nordmänner","587376412625731614","442350475950424104","587649118978179072",'''Willkommen bei den Dunkelrittern!:racehorse:
+Du bist einer dunklen Fraktion beigetreten welche in der nord-östlichen Zone des Kontinents lebt.
+Verhalte dich wie ein Dunkelritter du kannst natürlich auch ein Knappe oder ein Magier sein! :black_heart:
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Ritter/Knappe/Magier benehmen müsst!
+Viel Spaß im Projekt! Auf eine dunkle Zeit!:racehorse:'''),
+              ("Wilder Bergstamm","587376373849522257", "184385677301907456", "587648048641867776",'''Willkommen beim Wilden Bergstamm!:mount_fuji:
+Du bist einer wilden Fraktion beigetreten welche in der nördlichen Zone des Kontinents lebt.
+Verhalte dich wie ein Bewohner eines Wilden Stammes! :bear:
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Wilder benehmen müsst!
+Viel Spaß im Projekt! Auf ein bergiges Fest!:mount_fuji:'''),
+              ("Piraten","587376215162355743","231855253564162049","587648225360740384",'''Willkommen bei dem mystischen Orden!:church:
+Du bist einer geheimnisvollen Fraktion beigetreten welche in der Mitte des Kontinents lebt.
+Verhalte dich entweder wie ein Händler Söldner oder Priester! ✝️
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Händler/Söldner/Priester benehmen müsst!
+Auf ein spannendes Projekt!:church:'''),
+              ("Ägypter","587376337077927956","534021186439610389","587648422761463818",'''Willkommen bei den Nordmännern!❄️
+Du bist einer eisigen Fraktion beigetreten welche im Norden des Kontinents lebt.
+Verhalte dich wie ein Nordmann! :santa: 
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Nordmann benehmen müsst!
+Viel Spaß im Projekt! Auf ein eisiges Fest! ❄'''),
+              ("Ureinwohner","587376464815456398","264177403188871178","587648931920347136",'''Willkommen bei den Piraten!:skull:
+Du bist einer trinkenden Fraktion beigetreten welche außerhalb des Kontinents auf einer steinigen Insel lebt.
+Verhalte dich wie ein Pirat!
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Pirat benehmen müsst!
+Sonst geht es auf die Planke!
+Viel Spaß im Projekt! Arrr!:skull:'''),
+              ("Mystischer Orden","587376621846265896","367732762419003403","587648869551308801",'''Willkommen bei den Ägyptern!:sun_with_face:
+Du bist einer weisen Fraktion beigetreten welche im Süden des Kontinents lebt.
+Verhalte dich wie ein Ägypter! :small_red_triangle:
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Ägypter benehmen müsst!
+Viel Spaß im Projekt! Die Pyramiden sind mit dir!:sun_with_face:'''),
+              ("Römer","587376695691051017","339098056408432640","587648819680903171",'''Willkommen bei den Ureinwohnern!:palm_tree:
+Du bist einer alten und wilden Fraktion beigetreten welche in der süd-westlichen Zone des Kontinents lebt.
+Verhalte dich wie ein Ureinwohner! :see_no_evil:
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Ureinwohner benehmen müsst!
+Viel Spaß im Projekt! Gepriesen sei der Baum!:palm_tree:'''),
+              ("Dunkelritter","587376791170187274","351634859082645505","587648591548383232",'''Willkommen bei den Samurai!:japanese_castle:
+Du bist einer edlen Fraktion beigetreten welche im Westen des Kontinents lebt.
+Verhalte dich wie ein Samurai! :dragon:
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Samurai benehmen müsst!
+Viel Spaß im Projekt! Auf das Jahr des Hundes!:japanese_castle:'''),
+              ("Samurai","587376876184666123","220558436037820417","587648705990098947",'''Willkommen bei den Römern!:ocean:
+Du bist einer eigentlich friedlichen Fraktion beigetreten welche im süd-östlichen des Kontinents lebt.
+Verhalte dich wie ein Römer!
+Das hier ist ein Fulltime Roleplay Projekt das heißt ihr spielt einen Charakter. 
+Welche Eigenschaften der Charakter hat ist euch überlassen. Ihr dürft also euch selbst spielen 
+ABER beachtet das ihr in einer Fraktion seid und ihr euch wie ein Römer benehmen müsst!
+Viel Spaß im Projekt! Auf eine friedliche oder düstere Zeit!:ocean:''')]
 
-cursor.executemany("INSERT INTO fraktionen VALUES (?,?,?,?)",fraktionen)
+cursor.executemany("INSERT INTO fraktionen VALUES (?,?,?,?,?)",fraktionen)
 
 festungen = [("1","Nordmänner"),
              ("Bergfort","Wilder Bergstamm"),
