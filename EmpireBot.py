65a8c97ebf49ed2fa72aee7e5ce18b84e76fb412
Berkant03 <51971304+Berkant03@users.noreply.github.com>
2019-06-20 14:16:44 +0200

    added conn.commit() everywhere necessary

--- EmpireBot.py
+++ EmpireBot.py
@@ -60,22 +60,26 @@ def log(person_name,person_id,command,channel_name,datum): #command zeigt die vo
     tag = datum[0]
     uhrzeit = datum[1][:5]
     cursor.execute("INSERT INTO logs VALUES (?,?,?,?,?,?)",[person_name,person_id,command,channel,tag,uhrzeit])
-
+    conn.commit()
+    
 def leiter_wechsel(fraktion,leiter_id): #Wenn sich der Leiter einer Stadt ändert
     global cursor
     cursor.execute("UPDATE fraktionen SET leiter_id = ? WHERE fraktion = ?",[leiter_id,fraktion])
-
+    conn.commit()
+    
 def festung_einnahme(festung,fraktion): #Wenn eine Basis eingenommen wird. "fraktion" ist der neue Besitzer der Stadt
     global cursor
     cursor.execute("UPDATE festungen SET fraktion = ? WHERE festung = ?",[fraktion,festung])
-
+    conn.commit()
+    
 def invasion_ankündigung(festung,angreifer_fraktion,verteidiger_fraktion,datum): #datum als datetime.datetime object
     global cursor
     datum = datum.split()
     tag = datum[0]
     uhrzeit = datum[1][:5]
     cursor.execute("INSERT INTO invasion VALUES (?,?,?,?,?)",[festung,angreifer_fraktion,verteidiger_fraktion,tag,uhrzeit])
-
+    conn.commit()
+    
 def festungen():
     global cursor
     cursor.execute("SELECT festung FROM festungen")
